e8be383e56738df33421b0c9ad2eab39ad9983f4
ZeqoTibo <thibaultderouin5@gmail.com>
2022-02-28 22:32:53 +0100

    Last update (Change background, add img for spell )

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -28,6 +28,7 @@ class HomeController extends AbstractController
     public function index(ChampionRepository $championRepository): Response
     {
         $allChamps = $championRepository->findAll();
+
         return $this->render('yone/home.html.twig', [
             'allChamps' => $allChamps,
         ]);
@@ -39,8 +40,22 @@ class HomeController extends AbstractController
     public function showChamp (string $champion, ManagerRegistry $managerRegistry): Response
     {
         $result = $managerRegistry->getRepository(Champion::class)->findOneBy(['id'=>$champion]);
+
+        $json = file_get_contents('https://ddragon.leagueoflegends.com/cdn/12.4.1/data/fr_FR/championFull.json');
+        $decode = json_decode($json);
+        $name = preg_replace('/[^A-Za-z0-9\-]/', '', $result->getName());
+        $info =$decode->{'data'}->{$name};
+        $first = $decode->{'data'}->{$name}->{'spells'}[0]->{'image'}->{'full'};
+        $second = $decode->{'data'}->{$name}->{'spells'}[1]->{'image'}->{'full'};
+        $third = $decode->{'data'}->{$name}->{'spells'}[2]->{'image'}->{'full'};
+        $quad = $decode->{'data'}->{$name}->{'spells'}[3]->{'image'}->{'full'};
         return $this->render('yone/champSelect.html.twig', [
             'champion' => $result,
+            'info' => $info,
+            'a' => $first,
+            'z' => $second,
+            'e' => $third,
+            'r' => $quad
         ]);
     }
 
