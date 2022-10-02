ff2d4a705f8532594830732ccf1492c40824ced2
thibault derouin <thibaultderouin5@gmail.com>
2022-10-02 13:17:06 +0200

    Update

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Champion;
+use App\Form\ContactType;
 use App\Repository\ChampionRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -50,15 +52,45 @@ class HomeController extends AbstractController
     }*/
 
     /**
+     * @Route("/contact", name="contact", methods={"GET"})
+     */
+    public function contact(Request $request): Response
+    {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Voir projet piscine, prendre mailer pas swift
+        }
+
+        return $this->render('contact/contact.html.twig', [
+            'devisForm' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/tips", name="tips", methods={"GET"})
+     */
+    public function tips(): Response
+    {
+        return $this->render('yone/tips.html.twig');
+    }
+
+    /**
      * @Route("/{champion}", name="showChamp", methods={"GET"})
      */
     public function showChamp(string $champion, ManagerRegistry $managerRegistry): Response
     {
+
         $result = $managerRegistry->getRepository(Champion::class)->findOneBy(['id' => $champion]);
 
         $json = file_get_contents('https://ddragon.leagueoflegends.com/cdn/12.4.1/data/fr_FR/championFull.json');
         $decode = json_decode($json);
+        if (!$result){
+            return $this->render('bundles/TwigBundle/Exception/error500.html.twig');
+        }
         $name = preg_replace('/[^A-Za-z0-9\-]/', '', $result->getName());
+
         if($name == "Wukong"){
             $info = $decode->{'data'}->{'MonkeyKing'};
         } else if ($name == "VelKoz"){
@@ -66,6 +98,7 @@ class HomeController extends AbstractController
         } else {
             $info = $decode->{'data'}->{$name};
         }
+
         return $this->render('yone/champSelect.html.twig', [
             'champion' => $result,
             'info' => $info,
